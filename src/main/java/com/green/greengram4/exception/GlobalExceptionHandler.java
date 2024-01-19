91336ff2f5b9e81ecf88f9a80886a8176385f9f6
TH <kareme007@gmail.com>
2024-01-19 11:12:15 +0900

    24.01.19.

--- src/main/java/com/green/greengram4/exception/GlobalExceptionHandler.java
+++ src/main/java/com/green/greengram4/exception/GlobalExceptionHandler.java
@@ -1,14 +1,29 @@
 package com.green.greengram4.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        List<String> errors = e.getBindingResult().getFieldErrors().stream().map(x-> x.getDefaultMessage()).toList();
+        Map<String, String> body = e.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(x -> x.getField(), y -> y.getDefaultMessage()));
+        body.put("code", status.getReasonPhrase());
+        return new ResponseEntity<>(body, status);
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleIIllegalArgumentException(IllegalArgumentException e) {
