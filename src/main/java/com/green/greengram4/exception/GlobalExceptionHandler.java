17cb963d65e57b6d592ee0f2a7ee040078662ce8
TH <kareme007@gmail.com>
2024-01-22 11:13:00 +0900

    24.01.22.

--- src/main/java/com/green/greengram4/exception/GlobalExceptionHandler.java
+++ src/main/java/com/green/greengram4/exception/GlobalExceptionHandler.java
@@ -1,40 +1,27 @@
 package com.green.greengram4.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        List<String> errors = e.getBindingResult().getFieldErrors().stream().map(x-> x.getDefaultMessage()).toList();
-        Map<String, String> body = e.getBindingResult().getFieldErrors().stream()
-                .collect(Collectors.toMap(x -> x.getField(), y -> y.getDefaultMessage()));
-        body.put("code", status.getReasonPhrase());
-        return new ResponseEntity<>(body, status);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> handleIIllegalArgumentException(IllegalArgumentException e) {
-        log.warn("handleIIllegalArgument", e);
-        return handleExceptionInternal(CommonErrorCode.INVALID_PARAMETER, CommonErrorCode.INVALID_PARAMETER.getMessage());
-    }
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleException(Exception e) {
         log.warn("handleException", e);
-        return handleExceptionInternal(CommonErrorCode.INTERNAL_SERVER_ERROR);  //
+        return handleExceptionInternal(CommonErrorCode.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(RestApiException.class)
@@ -43,14 +30,22 @@ public class GlobalExceptionHandler {
         return handleExceptionInternal(e.getErrorCode());
     }
 
-    public ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
+    // @Valid 어노테이션으로 넘어오는 에러 처리
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return handleExceptionInternal(ex, CommonErrorCode.INVALID_PARAMETER);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
         return handleExceptionInternal(errorCode, null);
     }
 
-    public ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message) {
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode
+            , String message) {
         return ResponseEntity.status(errorCode.getHttpStatus())
-                .body(message == null ?
-                          makeErrorResponse(errorCode)
+                .body(message == null
+                        ? makeErrorResponse(errorCode)
                         : makeErrorResponse(errorCode, message));
     }
 
@@ -62,6 +57,27 @@ public class GlobalExceptionHandler {
         return ErrorResponse.builder()
                 .code(errorCode.name())
                 .message(message)
+                .build();
+    }
+
+    // @Valid 어노테이션으로 넘어오는 에러 처리 메세지를 보내기 위한 메소드
+    private ResponseEntity<Object> handleExceptionInternal(BindException e, ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(makeErrorResponse(e, errorCode));
+    }
+
+    // 코드 가독성을 위해 에러 처리 메세지를 만드는 메소드 분리
+    private ErrorResponse makeErrorResponse(BindException e, ErrorCode errorCode) {
+        List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(ErrorResponse.ValidationError::of)
+                .collect(Collectors.toList());
+
+        return ErrorResponse.builder()
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .valid(validationErrorList)
                 .build();
     }
 }
