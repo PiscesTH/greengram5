f0e6e5e8aa93220cd1afe7dee4cee4b53384998b
TH <kareme007@gmail.com>
2024-02-08 12:45:11 +0900

    24.02.08. social login setting

--- src/main/java/com/green/greengram4/security/oauth2/userinfo/CustomOAuth2UserService.java
+++ src/main/java/com/green/greengram4/security/oauth2/userinfo/CustomOAuth2UserService.java
@@ -1,13 +1,44 @@
 package com.green.greengram4.security.oauth2.userinfo;
 
+import com.green.greengram4.security.oauth2.SocialProviderType;
 import com.green.greengram4.user.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final UserMapper mapper;
     private final OAuth2UserInfoFactory factory;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User user = super.loadUser(userRequest);
+        try {
+            return this.process(userRequest, user);
+        } catch (AuthenticationException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new InternalAuthenticationServiceException(e.getMessage(), e.getCause());
+        }
+    }
+
+    private OAuth2User process(OAuth2UserRequest userRequest, OAuth2User user) {
+//        userRequest.getClientRegistration().getRegistrationId()   -> ex)naver / kakao
+        SocialProviderType socialProviderType = SocialProviderType.valueOf(userRequest.getClientRegistration()
+                .getRegistrationId()
+                .toUpperCase());
+
+        Map<String, Object> attributes = user.getAttributes();
+        OAuth2UserInfo oAuth2UserInfo = factory.getOAuth2UserInfo(socialProviderType, attributes);
+        return null;
+    }
 }
