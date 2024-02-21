65cafee0a3561e23cc42d32248c3b35922f65abe
TH <kareme007@gmail.com>
2024-02-21 18:10:36 +0900

    24.02.21. jpa query dsl

--- src/main/java/com/green/greengram4/feed/FeedQdslRepositoryImpl.java
+++ src/main/java/com/green/greengram4/feed/FeedQdslRepositoryImpl.java
@@ -2,14 +2,13 @@ package com.green.greengram4.feed;
 
 import com.green.greengram4.entity.FeedEntity;
 import com.green.greengram4.entity.FeedPicsEntity;
-import com.green.greengram4.entity.UserEntity;
 import com.green.greengram4.feed.model.FeedSelVo;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 import static com.green.greengram4.entity.QFeedEntity.feedEntity;
